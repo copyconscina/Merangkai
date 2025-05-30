02108609e1929a60c570ab61b6fd09fa7de20966
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 03:02:11 +0700

    update landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
-/** @type {import('tailwindcss').Config */
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
+    "./about.html",
+    "./services.html",
+    "./cases.html",
+    "./team.html",
+    "./contact.html",
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cinzel: ['Cinzel', 'serif'],
+        lora: ['Lora', 'serif'],
+      },
+      colors: {
+        orange: {
+          400: '#fb923c',
+          500: '#f97316'
+        },
+        blue: {
+          900: '#1e3a8a'
+        }
+      }
+    },
   },
   plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './about.html',
+        services: './services.html',
+        cases: './cases.html',
+        team: './team.html',
+        contact: './contact.html'
+      }
+    }
+  }
 });
