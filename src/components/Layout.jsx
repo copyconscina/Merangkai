7793ed51fadb0b7036f7923d5f413661bd018fc4
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 03:38:24 +0700

    update landing page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layout.jsx';
+import Gallery from './Gallery';
 import { motion } from 'framer-motion';
 import Gallery from './components/Gallery.jsx';
 
