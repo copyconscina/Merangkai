7f348855ff2ac2c8407246c607bcdad471636217
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 02:37:28 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // jika kamu pakai React
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/merangkai1/', // WAJIB untuk GitHub Pages
   plugins: [react()],
-})
+  base: './',
+});
