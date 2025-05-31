26544224202b75bd833bfa470020f6267c5065b7
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 09:00:00 +0700

    update landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-    content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-       animation: {
-        'float-slow': 'float 12s ease-in-out infinite',
-        'float-medium': 'float 8s ease-in-out infinite',
-        'float-fast': 'float 6s ease-in-out infinite',
+      animation: {
+        'float-slow': 'float 18s ease-in-out infinite',
+        'float-medium': 'float 12s ease-in-out infinite',
+        'float-fast': 'float 8s ease-in-out infinite',
       },
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0) translateX(0)' },
-          '50%': { transform: 'translateY(-20px) translateX(10px)' },
+          '50%': { transform: 'translateY(-30px) translateX(20px)' },
         },
       },
     },
