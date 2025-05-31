28df9ac56e01487eed595ce500aec824ce925aca
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 08:21:13 +0700

    update landing page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
-const Services: React.FC = () => {
+const Services = () => {
   const services = [
     {
       title: "Mindful Experiences",
