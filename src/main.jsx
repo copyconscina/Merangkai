a53741110fbaf997854d2668bfff9fd380795d2d
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 09:32:46 +0700

    update landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
+import App from './App.jsx'; // Make sure this path is correct
+import './index.css'; // Make sure this path is correct for your global CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>,
 );
