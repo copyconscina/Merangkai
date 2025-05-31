d90fb074fff3b06c8013caf97461ed96c9ff656c
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 11:36:14 +0700

    update landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout'; // Your Layout import
-import Home from './pages/Home';
-// ... other page imports ...
+import Layout from './components/Layout.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Services from './pages/Services.jsx';
+import Gallery from './components/Gallery.jsx';
+import Contact from './pages/Contact.jsx';
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* ... other routes ... */}
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
     </Router>
   );
 }
-
-export default App;
