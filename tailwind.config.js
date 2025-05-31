25c5939bb45ebd52ef89d7337b8fd2fe9a844b3b
erzaikhsan <ikhsanierza@gmail.com>
2025-05-31 12:03:35 +0700

    update landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        serif: ['Cinzel', 'serif'],
-      },
       animation: {
-  move1: 'move1 30s ease-in-out infinite',
-  move2: 'move2 35s ease-in-out infinite',
-  move3: 'move3 40s ease-in-out infinite',
-},
-keyframes: {
-  move1: {
-    '0%, 100%': { transform: 'translate(0, 0)' },
-    '50%': { transform: 'translate(100px, -150px)' },
-  },
-  move2: {
-    '0%, 100%': { transform: 'translate(0, 0)' },
-    '50%': { transform: 'translate(-120px, 100px)' },
-  },
-  move3: {
-    '0%, 100%': { transform: 'translate(0, 0)' },
-    '50%': { transform: 'translate(200px, 100px)' },
-  },
-}
-
+        move1: 'move1 30s ease-in-out infinite',
+        move2: 'move2 35s ease-in-out infinite',
+        move3: 'move3 40s ease-in-out infinite',
+      },
+      keyframes: {
+        move1: {
+          '0%, 100%': { transform: 'translate(0, 0)' },
+          '50%': { transform: 'translate(100px, -150px)' },
+        },
+        move2: {
+          '0%, 100%': { transform: 'translate(0, 0)' },
+          '50%': { transform: 'translate(-120px, 100px)' },
+        },
+        move3: {
+          '0%, 100%': { transform: 'translate(0, 0)' },
+          '50%': { transform: 'translate(200px, 100px)' },
+        },
+      },
     },
   },
   plugins: [],
